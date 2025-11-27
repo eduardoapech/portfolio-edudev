b842f9592e3702dec2e3acce7fd9702623cffe45
eduardoapech <eduardo.augusto.pech97@gmail.comh>
2025-11-27 11:55:25 -0300

    fix: atualização

--- src/components/organisms/HeroSection/index.jsx
+++ src/components/organisms/HeroSection/index.jsx
@@ -1,5 +1,5 @@
-import { useRef, useState, useEffect } from 'react'
-import { StyledHeroSection, StyledHeroDetail, StyledSubtitle, StyledSocialIcons } from './styles'
+import { useRef, useEffect } from 'react'
+import { StyledHeroSection, StyledHeroDetail, StyledSocialIcons } from './styles'
 import Title from '../../atoms/Title'
 import SocialLink from '../../molecules/SocialLink'
 import { useTypeEffect } from '../../../hooks/useTypeEffect'
@@ -8,28 +8,18 @@ import { getTranslation } from '../../../locales'
 
 const HeroSection = () => {
   const titleRef = useRef(null)
-  const subtitleRef = useRef(null)
-  const [startSubtitle, setStartSubtitle] = useState(false)
   const { language } = useLanguage()
   const t = (key) => getTranslation(language, key)
 
   const heroTitle = t('hero.title')
-  const heroSubtitle = t('hero.subtitle')
 
   useEffect(() => {
     if (titleRef.current) {
       titleRef.current.innerText = ''
-      setStartSubtitle(false)
     }
   }, [language])
 
-  useTypeEffect(titleRef, heroTitle, 100, () => {
-    setTimeout(() => {
-      setStartSubtitle(true)
-    }, 300)
-  })
-
-  useTypeEffect(subtitleRef, startSubtitle ? heroSubtitle : '', 100)
+  useTypeEffect(titleRef, heroTitle, 100)
 
   const technologies = [
     { title: 'Java', iconClass: 'ri-java-fill' },
@@ -44,7 +34,6 @@ const HeroSection = () => {
     <StyledHeroSection>
       <StyledHeroDetail>
         <Title ref={titleRef} minHeight="60px" id="titulo-principal" />
-        <StyledSubtitle ref={subtitleRef} id="subtitulo" />
         <StyledSocialIcons>
           {technologies.map((tech) => (
             <SocialLink key={tech.title} title={tech.title} iconClass={tech.iconClass} />
